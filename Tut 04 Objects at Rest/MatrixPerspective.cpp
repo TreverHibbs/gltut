dd9e5cc40657102ad01ff01f058b4e1873e7ecc3
Trever Hibbs <treverhibbs@gmail.com>
2026-01-22 10:02:31 -0800

    add tut 4 edits

--- Tut 04 Objects at Rest/MatrixPerspective.cpp
+++ Tut 04 Objects at Rest/MatrixPerspective.cpp
@@ -8,220 +8,209 @@
 #include <GL/freeglut.h>
 #include "../framework/framework.h"
 
-#define ARRAY_COUNT( array ) (sizeof( array ) / (sizeof( array[0] ) * (sizeof( array ) != sizeof(void*) || sizeof( array[0] ) <= sizeof(void*))))
+#define ARRAY_COUNT(array)                                                     \
+  (sizeof(array) / (sizeof(array[0]) * (sizeof(array) != sizeof(void *) ||     \
+                                        sizeof(array[0]) <= sizeof(void *))))
 
 GLuint theProgram;
 
 GLuint offsetUniform;
 GLuint perspectiveMatrixUnif;
 
-void InitializeProgram()
-{
-	std::vector<GLuint> shaderList;
+float theMatrix[16];
+float fFrustumScale = 0.8f;
 
-	shaderList.push_back(Framework::LoadShader(GL_VERTEX_SHADER, "MatrixPerspective.vert"));
-	shaderList.push_back(Framework::LoadShader(GL_FRAGMENT_SHADER, "StandardColors.frag"));
+void InitializeProgram() {
+  std::vector<GLuint> shaderList;
 
-	theProgram = Framework::CreateProgram(shaderList);
+  shaderList.push_back(
+      Framework::LoadShader(GL_VERTEX_SHADER, "MatrixPerspective.vert"));
+  shaderList.push_back(
+      Framework::LoadShader(GL_FRAGMENT_SHADER, "StandardColors.frag"));
 
-	offsetUniform = glGetUniformLocation(theProgram, "offset");
+  theProgram = Framework::CreateProgram(shaderList);
 
-	perspectiveMatrixUnif = glGetUniformLocation(theProgram, "perspectiveMatrix");
+  offsetUniform = glGetUniformLocation(theProgram, "offset");
 
-	float fFrustumScale = 1.0f; float fzNear = 0.5f; float fzFar = 3.0f;
+  perspectiveMatrixUnif = glGetUniformLocation(theProgram, "perspectiveMatrix");
 
-	float theMatrix[16];
-	memset(theMatrix, 0, sizeof(float) * 16);
+  float fzNear = 0.5f;
+  float fzFar = 3.0f;
 
-	theMatrix[0] = fFrustumScale;
-	theMatrix[5] = fFrustumScale;
-	theMatrix[10] = (fzFar + fzNear) / (fzNear - fzFar);
-	theMatrix[14] = (2 * fzFar * fzNear) / (fzNear - fzFar);
-	theMatrix[11] = -1.0f;
+  memset(theMatrix, 0, sizeof(float) * 16);
 
-	glUseProgram(theProgram);
-	glUniformMatrix4fv(perspectiveMatrixUnif, 1, GL_FALSE, theMatrix);
-	glUseProgram(0);
+  theMatrix[0] = fFrustumScale;
+  theMatrix[5] = fFrustumScale;
+  theMatrix[10] = (fzFar + fzNear) / (fzNear - fzFar);
+  theMatrix[14] = (2 * fzFar * fzNear) / (fzNear - fzFar);
+  theMatrix[11] = -1.0f;
+
+  glUseProgram(theProgram);
+  glUniformMatrix4fv(perspectiveMatrixUnif, 1, GL_FALSE, theMatrix);
+  glUseProgram(0);
 }
 
 const float vertexData[] = {
-	 0.25f,  0.25f, -1.25f, 1.0f,
-	 0.25f, -0.25f, -1.25f, 1.0f,
-	-0.25f,  0.25f, -1.25f, 1.0f,
+    // clang-format off
+    0.25f,  0.25f,  -1.25f, 1.0f,
+    0.25f,  -0.25f, -1.25f, 1.0f,
+    -0.25f, 0.25f,  -1.25f, 1.0f,
+    0.25f,  -0.25f, -1.25f, 1.0f,
+    -0.25f, -0.25f, -1.25f, 1.0f,
+    -0.25f, 0.25f,  -1.25f, 1.0f,
+    0.25f,  0.25f,  -2.75f, 1.0f,
+    -0.25f, 0.25f, -2.75f, 1.0f,
+    0.25f,  -0.25f, -2.75f, 1.0f,
+    0.25f,  -0.25f, -2.75f, 1.0f,
+    -0.25f, 0.25f, -2.75f, 1.0f,
+    -0.25f, -0.25f, -2.75f, 1.0f,
+    -0.25f, 0.25f,  -1.25f, 1.0f,
+    -0.25f, -0.25f, -1.25f, 1.0f,
+    -0.25f, -0.25f, -2.75f, 1.0f,
+    -0.25f, 0.25f,  -1.25f, 1.0f,   -0.25f, -0.25f,
+    -2.75f, 1.0f,   -0.25f, 0.25f,  -2.75f, 1.0f,
 
-	 0.25f, -0.25f, -1.25f, 1.0f,
-	-0.25f, -0.25f, -1.25f, 1.0f,
-	-0.25f,  0.25f, -1.25f, 1.0f,
+    0.25f,  0.25f,  -1.25f, 1.0f,   0.25f,  -0.25f,
+    -2.75f, 1.0f,   0.25f,  -0.25f, -1.25f, 1.0f,
 
-	 0.25f,  0.25f, -2.75f, 1.0f,
-	-0.25f,  0.25f, -2.75f, 1.0f,
-	 0.25f, -0.25f, -2.75f, 1.0f,
+    0.25f,  0.25f,  -1.25f, 1.0f,   0.25f,  0.25f,
+    -2.75f, 1.0f,   0.25f,  -0.25f, -2.75f, 1.0f,
 
-	 0.25f, -0.25f, -2.75f, 1.0f,
-	-0.25f,  0.25f, -2.75f, 1.0f,
-	-0.25f, -0.25f, -2.75f, 1.0f,
+    0.25f,  0.25f,  -2.75f, 1.0f,   0.25f,  0.25f,
+    -1.25f, 1.0f,   -0.25f, 0.25f,  -1.25f, 1.0f,
 
-	-0.25f,  0.25f, -1.25f, 1.0f,
-	-0.25f, -0.25f, -1.25f, 1.0f,
-	-0.25f, -0.25f, -2.75f, 1.0f,
+    0.25f,  0.25f,  -2.75f, 1.0f,   -0.25f, 0.25f,
+    -1.25f, 1.0f,   -0.25f, 0.25f,  -2.75f, 1.0f,
 
-	-0.25f,  0.25f, -1.25f, 1.0f,
-	-0.25f, -0.25f, -2.75f, 1.0f,
-	-0.25f,  0.25f, -2.75f, 1.0f,
+    0.25f,  -0.25f, -2.75f, 1.0f,   -0.25f, -0.25f,
+    -1.25f, 1.0f,   0.25f,  -0.25f, -1.25f, 1.0f,
 
-	 0.25f,  0.25f, -1.25f, 1.0f,
-	 0.25f, -0.25f, -2.75f, 1.0f,
-	 0.25f, -0.25f, -1.25f, 1.0f,
+    0.25f,  -0.25f, -2.75f, 1.0f,   -0.25f, -0.25f,
+    -2.75f, 1.0f,   -0.25f, -0.25f, -1.25f, 1.0f,
 
-	 0.25f,  0.25f, -1.25f, 1.0f,
-	 0.25f,  0.25f, -2.75f, 1.0f,
-	 0.25f, -0.25f, -2.75f, 1.0f,
+    0.0f,   0.0f,   1.0f,   1.0f,   0.0f,   0.0f,
+    1.0f,   1.0f,   0.0f,   0.0f,   1.0f,   1.0f,
 
-	 0.25f,  0.25f, -2.75f, 1.0f,
-	 0.25f,  0.25f, -1.25f, 1.0f,
-	-0.25f,  0.25f, -1.25f, 1.0f,
+    0.0f,   0.0f,   1.0f,   1.0f,   0.0f,   0.0f,
+    1.0f,   1.0f,   0.0f,   0.0f,   1.0f,   1.0f,
 
-	 0.25f,  0.25f, -2.75f, 1.0f,
-	-0.25f,  0.25f, -1.25f, 1.0f,
-	-0.25f,  0.25f, -2.75f, 1.0f,
+    0.8f,   0.8f,   0.8f,   1.0f,   0.8f,   0.8f,
+    0.8f,   1.0f,   0.8f,   0.8f,   0.8f,   1.0f,
 
-	 0.25f, -0.25f, -2.75f, 1.0f,
-	-0.25f, -0.25f, -1.25f, 1.0f,
-	 0.25f, -0.25f, -1.25f, 1.0f,
+    0.8f,   0.8f,   0.8f,   1.0f,   0.8f,   0.8f,
+    0.8f,   1.0f,   0.8f,   0.8f,   0.8f,   1.0f,
 
-	 0.25f, -0.25f, -2.75f, 1.0f,
-	-0.25f, -0.25f, -2.75f, 1.0f,
-	-0.25f, -0.25f, -1.25f, 1.0f,
+    0.0f,   1.0f,   0.0f,   1.0f,   0.0f,   1.0f,
+    0.0f,   1.0f,   0.0f,   1.0f,   0.0f,   1.0f,
 
+    0.0f,   1.0f,   0.0f,   1.0f,   0.0f,   1.0f,
+    0.0f,   1.0f,   0.0f,   1.0f,   0.0f,   1.0f,
 
+    0.5f,   0.5f,   0.0f,   1.0f,   0.5f,   0.5f,
+    0.0f,   1.0f,   0.5f,   0.5f,   0.0f,   1.0f,
 
+    0.5f,   0.5f,   0.0f,   1.0f,   0.5f,   0.5f,
+    0.0f,   1.0f,   0.5f,   0.5f,   0.0f,   1.0f,
 
-	0.0f, 0.0f, 1.0f, 1.0f,
-	0.0f, 0.0f, 1.0f, 1.0f,
-	0.0f, 0.0f, 1.0f, 1.0f,
+    1.0f,   0.0f,   0.0f,   1.0f,   1.0f,   0.0f,
+    0.0f,   1.0f,   1.0f,   0.0f,   0.0f,   1.0f,
 
-	0.0f, 0.0f, 1.0f, 1.0f,
-	0.0f, 0.0f, 1.0f, 1.0f,
-	0.0f, 0.0f, 1.0f, 1.0f,
+    1.0f,   0.0f,   0.0f,   1.0f,   1.0f,   0.0f,
+    0.0f,   1.0f,   1.0f,   0.0f,   0.0f,   1.0f,
 
-	0.8f, 0.8f, 0.8f, 1.0f,
-	0.8f, 0.8f, 0.8f, 1.0f,
-	0.8f, 0.8f, 0.8f, 1.0f,
+    0.0f,   1.0f,   1.0f,   1.0f,   0.0f,   1.0f,
+    1.0f,   1.0f,   0.0f,   1.0f,   1.0f,   1.0f,
 
-	0.8f, 0.8f, 0.8f, 1.0f,
-	0.8f, 0.8f, 0.8f, 1.0f,
-	0.8f, 0.8f, 0.8f, 1.0f,
-
-	0.0f, 1.0f, 0.0f, 1.0f,
-	0.0f, 1.0f, 0.0f, 1.0f,
-	0.0f, 1.0f, 0.0f, 1.0f,
-
-	0.0f, 1.0f, 0.0f, 1.0f,
-	0.0f, 1.0f, 0.0f, 1.0f,
-	0.0f, 1.0f, 0.0f, 1.0f,
-
-	0.5f, 0.5f, 0.0f, 1.0f,
-	0.5f, 0.5f, 0.0f, 1.0f,
-	0.5f, 0.5f, 0.0f, 1.0f,
-
-	0.5f, 0.5f, 0.0f, 1.0f,
-	0.5f, 0.5f, 0.0f, 1.0f,
-	0.5f, 0.5f, 0.0f, 1.0f,
-
-	1.0f, 0.0f, 0.0f, 1.0f,
-	1.0f, 0.0f, 0.0f, 1.0f,
-	1.0f, 0.0f, 0.0f, 1.0f,
-
-	1.0f, 0.0f, 0.0f, 1.0f,
-	1.0f, 0.0f, 0.0f, 1.0f,
-	1.0f, 0.0f, 0.0f, 1.0f,
-
-	0.0f, 1.0f, 1.0f, 1.0f,
-	0.0f, 1.0f, 1.0f, 1.0f,
-	0.0f, 1.0f, 1.0f, 1.0f,
-
-	0.0f, 1.0f, 1.0f, 1.0f,
-	0.0f, 1.0f, 1.0f, 1.0f,
-	0.0f, 1.0f, 1.0f, 1.0f,
-
+    0.0f,   1.0f,   1.0f,   1.0f,   0.0f,   1.0f,
+    1.0f,   1.0f,   0.0f,   1.0f,   1.0f,   1.0f,
+    // clang-format on
 };
 
 GLuint vertexBufferObject;
 GLuint vao;
 
+void InitializeVertexBuffer() {
+  glGenBuffers(1, &vertexBufferObject);
 
-void InitializeVertexBuffer()
-{
-	glGenBuffers(1, &vertexBufferObject);
-
-	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexData), vertexData, GL_STATIC_DRAW);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertexData), vertexData, GL_STATIC_DRAW);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
 
-//Called after the window and OpenGL are initialized. Called exactly once, before the main loop.
-void init()
-{
-	InitializeProgram();
-	InitializeVertexBuffer();
+// Called after the window and OpenGL are initialized. Called exactly once,
+// before the main loop.
+void init() {
+  InitializeProgram();
+  InitializeVertexBuffer();
 
-	glGenVertexArrays(1, &vao);
-	glBindVertexArray(vao);
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
 
-	glEnable(GL_CULL_FACE);
-	glCullFace(GL_BACK);
-	glFrontFace(GL_CW);
+  glEnable(GL_CULL_FACE);
+  glCullFace(GL_BACK);
+  glFrontFace(GL_CW);
 }
 
-//Called to update the display.
-//You should call glutSwapBuffers after all of your rendering to display what you rendered.
-//If you need continuous updates of the screen, call glutPostRedisplay() at the end of the function.
-void display()
-{
-	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
+// Called to update the display.
+// You should call glutSwapBuffers after all of your rendering to display what
+// you rendered. If you need continuous updates of the screen, call
+// glutPostRedisplay() at the end of the function.
+void display() {
+  glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+  glClear(GL_COLOR_BUFFER_BIT);
 
-	glUseProgram(theProgram);
+  glUseProgram(theProgram);
 
-	glUniform2f(offsetUniform, 0.5f, 0.5f);
+  glUniform2f(offsetUniform, 0.5f, 0.5f);
 
-	size_t colorData = sizeof(vertexData) / 2;
-	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
-	glEnableVertexAttribArray(0);
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, 0);
-	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, (void*)colorData);
+  size_t colorData = sizeof(vertexData) / 2;
+  glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
+  glEnableVertexAttribArray(0);
+  glEnableVertexAttribArray(1);
+  glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, 0);
+  glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, (void *)colorData);
 
-	glDrawArrays(GL_TRIANGLES, 0, 36);
+  glDrawArrays(GL_TRIANGLES, 0, 36);
 
-	glDisableVertexAttribArray(0);
-	glDisableVertexAttribArray(1);
-	glUseProgram(0);
+  glDisableVertexAttribArray(0);
+  glDisableVertexAttribArray(1);
+  glUseProgram(0);
 
-	glutSwapBuffers();
-	glutPostRedisplay();
+  glutSwapBuffers();
+  glutPostRedisplay();
 }
 
-//Called whenever the window is resized. The new window size is given, in pixels.
-//This is an opportunity to call glViewport or glScissor to keep up with the change in size.
-void reshape (int w, int h)
-{
-	glViewport(0, 0, (GLsizei) w, (GLsizei) h);
+// Called whenever the window is resized. The new window size is given, in
+// pixels. This is an opportunity to call glViewport or glScissor to keep up
+// with the change in size.
+void reshape(int w, int h) {
+  if (w < h)
+    glViewport(0, 0, (GLsizei)w, (GLsizei)w);
+  else
+    glViewport(0, 0, (GLsizei)h, (GLsizei)h);
+
+  // TODO I still don't feel like I understnad how to move the frustum plane
+  // Find an answer for this
+  glUseProgram(theProgram);
+  theMatrix[11] = h - 501;
+  theMatrix[12] = abs(h - 501);
+  theMatrix[13] = abs(h - 501);
+  glUniformMatrix4fv(perspectiveMatrixUnif, 1, GL_FALSE, theMatrix);
+  glUseProgram(0);
 }
 
-//Called whenever a key on the keyboard was pressed.
-//The key is given by the ''key'' parameter, which is in ASCII.
-//It's often a good idea to have the escape key (ASCII value 27) call glutLeaveMainLoop() to 
-//exit the program.
-void keyboard(unsigned char key, int x, int y)
-{
-	switch (key)
-	{
-	case 27:
-		glutLeaveMainLoop();
-		return;
-	}
+// Called whenever a key on the keyboard was pressed.
+// The key is given by the ''key'' parameter, which is in ASCII.
+// It's often a good idea to have the escape key (ASCII value 27) call
+// glutLeaveMainLoop() to exit the program.
+void keyboard(unsigned char key, int x, int y) {
+  switch (key) {
+  case 27:
+    glutLeaveMainLoop();
+    return;
+  }
 }
 
-
-unsigned int defaults(unsigned int displayMode, int &width, int &height) {return displayMode;}
+unsigned int defaults(unsigned int displayMode, int &width, int &height) {
+  return displayMode;
+}
